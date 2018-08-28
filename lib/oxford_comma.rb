8dcc9b4c02b548a66a942ac1e55fe036806f27bd
Nick Romans <nicholas.romans@gmail.com>
2018-08-28 17:45:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.length == 1
   array.join
 elsif array.length == 2 
 array.join(" and ")
-elsif array >= 3
+elsif array.length >= 3
 new_array_end = "and #{array[-1]}"
 array.pop
 array << new_array_end
